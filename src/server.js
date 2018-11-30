073aace494ac9c7582e9f5fee22295ebce735e74
Grataloup Thibault <Thibault.GRATALOUP@etu.uca.fr>
2018-11-30 15:32:08 +0100

    Maj2 du port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express()
-//const models = require('./models/index');
+const models = require('./models/index');
 
 // Decode json and x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,11 +11,8 @@ app.use(bodyParser.json())
 // Add a bit of logging
 app.use(morgan('short'))
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-/*
+
 // Get all the users defined
 app.get('/', function (req, res) {
   models.User.findAll()
@@ -32,10 +29,10 @@ app.post('/', function(req, res) {
     .then(() => {
       res.send('User added !')
     })
-})*/
+})
 
 // Synchronize models
-//models.sequelize.sync().then(function() {
+models.sequelize.sync().then(function() {
   /**
    * Listen on provided port, on all network interfaces.
    * 
